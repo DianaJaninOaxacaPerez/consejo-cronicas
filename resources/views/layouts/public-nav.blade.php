edc586e313a15d35e170831258ee2859e2e8bd8c
Diana Janin <dianaoaxaca32@gmail.com>
2026-08-11 13:25:20 -0600

    Corrige ruta evento.qr por evento.confirmar.form y agrega enlace a Plataformas en el navbar

--- resources/views/layouts/public-nav.blade.php
+++ resources/views/layouts/public-nav.blade.php
@@ -22,7 +22,8 @@
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('entrevistas') }}">Entrevistas</a></li>
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('perfiles') }}">Perfiles</a></li>
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('videos') }}">Videos</a></li>
-        <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('evento.qr') }}">Evento</a></li>
+        <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('plataformas') }}">Plataformas</a></li>
+        <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('evento.confirmar.form') }}">Evento</a></li>
         </ul>
 
         <div class="menu-title d-lg-none">CRÓNICA HUEJUTLENSE</div>
