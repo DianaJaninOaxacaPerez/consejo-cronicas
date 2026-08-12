9064526693d85607c737fd572b3369adae418ea0
Jaire <jairehdez87@gmail.com>
2026-08-12 14:41:05 -0600

    Modif en plataformas

--- resources/views/layouts/public-nav.blade.php
+++ resources/views/layouts/public-nav.blade.php
@@ -21,6 +21,7 @@
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('noticias') }}">Noticias</a></li>
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('entrevistas') }}">Entrevistas</a></li>
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('perfiles') }}">Perfiles</a></li>
+        <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('plataformas') }}">Plataformas </a> </li>
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('videos') }}">Videos</a></li>
         <li class="nav-item"><a class="nav-link opcion-menu" href="{{ route('evento.confirmar.form') }}">Evento</a></li>
         </ul>
